5713a91faf5d0026cc9afd5e6cc2d5e1afe662f7
holksen <cheryxso@gmail.com>
2025-01-18 14:06:47 +0200

    Add FuncA

---
+++ FuncA.h
@@ -0,0 +1,13 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+	public:
+		double calculate(double x, int n);
+};
+
+#endif
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+	FuncA func;
+	std::cout << "FuncA result: " << func.calculate(1.0, 10) << std::endl;
+	return 0;
+}
+
+
